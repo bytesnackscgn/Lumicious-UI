1b463d84c02928d4171b60eacd0d565f94e6150b
Georgij Michel <g.michel@bytesnacks.de>
2026-06-19 22:25:39 +0200

    refactor(LText,LHeadline): restructured constants, utils, gradient styles, colors.

---
+++ src/constants/ui/color.ts
@@ -0,0 +1,21 @@
+export const solidColors: Record<string, string> = {
+  white: "text-white",
+  blue: "text-blue-400",
+  purple: "text-purple-400",
+  pink: "text-pink-400",
+  green: "text-green-400",
+  yellow: "text-yellow-400",
+  red: "text-red-400",
+  indigo: "text-indigo-400",
+};
+
+export const defaultCvaColorOptions = {
+  white: "",
+  blue: "",
+  purple: "",
+  pink: "",
+  green: "",
+  yellow: "",
+  red: "",
+  indigo: "",
+};
